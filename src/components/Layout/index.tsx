c5fa2c486018d1f36975158ebb54d40f1d3fceb7
Enzo Vieira <enzovieira16@hotmail.com>
2023-05-13 18:09:13 +0100

    add: Layout component (#3)

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import { Avatar, TextInput } from "@mantine/core";
+
+import { MdOutlineMap, MdSearch } from "react-icons/md";
+
+interface IProps {
+  children: ReactNode;
+}
+
+export function Layout({ children }: IProps) {
+  return (
+    <div>
+      <header className="h-[92px] flex items-center justify-between px-12 py-4">
+        <h1>LOGO</h1>
+
+        <TextInput
+          icon={<MdOutlineMap />}
+          size="md"
+          rightSection={
+            <button className="bg-black h-full flex justify-center items-center">
+              <MdSearch size={24} className="text-white w-[44px]" />
+            </button>
+          }
+          placeholder="Where you need trust?"
+        />
+
+        <Avatar
+          src="/profile-image.jpg"
+          alt="Profile Avatar"
+          radius="xl"
+          size="lg"
+        />
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+}
